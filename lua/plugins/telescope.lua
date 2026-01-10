3422e51539a26ae59298afdaff040867960962a8
layton <cheng_l@hotmail.com>
2026-01-10 02:28:38 -0500

    add markdown frontmatter search function

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,46 @@
+local function search_markdown_frontmatter()
+	local pickers = require("telescope.pickers")
+	local finders = require("telescope.finders")
+	local make_entry = require("telescope.make_entry")
+	local conf = require("telescope.config").values
+
+	local live_frontmatter = finders.new_job(function(prompt)
+		if not prompt or prompt == "" then
+			return nil
+		end
+
+		-- Parse input, e.g., "tags: movie"
+		local key, value = prompt:match("([^:]+):%s*(.*)")
+		local search_pattern
+
+		if key and value then
+			-- ^ matches start of line, .* matches anything in between
+			search_pattern = "^" .. vim.trim(key) .. ":.*" .. vim.trim(value)
+		else
+			search_pattern = "^" .. prompt
+		end
+
+		-- use 'sh -c' to pipe ripgrep output into awk for strict line filtering
+		-- --ignore-case makes the search case-insensitive
+		-- awk -F: '$2 <= 10' checks if the line number (column 2) is 10 or less
+		local command = string.format(
+			"rg --line-number --column --no-heading --color=never --ignore-case --type md -e %q | awk -F: '$2 <= 10'",
+			search_pattern
+		)
+
+		return { "sh", "-c", command }
+	end, make_entry.gen_from_vimgrep({}), nil)
+
+	pickers
+	    .new({}, {
+		    prompt_title = "Markdown Frontmatter Search (Lines 1-10, Case-Insensitive)",
+		    finder = live_frontmatter,
+		    previewer = conf.grep_previewer({}),
+		    sorter = conf.generic_sorter({}),
+	    })
+	    :find()
+end
+
 return {
 	{
 		"nvim-telescope/telescope.nvim",
@@ -9,6 +52,8 @@ return {
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
+			vim.keymap.set("n", "<leader>fm", search_markdown_frontmatter, { desc = "Telescope Frontmatter" })
 		end,
 	},
 	{
