38932886d9d1a2b449d781eca3ec9bb42918613c
layton <cheng_l@hotmail.com>
2026-04-02 04:25:07 -0400

    update nvim-treesitter to main branch to compatible with neovim 12

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,30 +1,23 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
 	build = ":TSUpdate",
 	config = function()
-		local configs = require("nvim-treesitter.configs")
-		configs.setup({
-			ensure_installed = {
-				"c",
-				"lua",
-				"vim",
-				"vimdoc",
-				"css",
-				"go",
-				"java",
-				"python",
-				"javascript",
-				"typescript",
-				"tsx",
-				"html",
-				"css",
-			},
-			sync_install = false,
-			highlight = {
-				enable = true,
-				disable = { "sql", "log" },
-			},
-			indent = { enable = true },
-		})
+		require("nvim-treesitter").install({
+			"rust",
+			"c",
+			"lua",
+			"vim",
+			"vimdoc",
+			"css",
+			"go",
+			"java",
+			"python",
+			"javascript",
+			"typescript",
+			"tsx",
+			"html",
+			"css",
+		}):wait(300000)
 	end,
 }
