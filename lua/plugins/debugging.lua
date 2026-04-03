59b5d2f2e07a1b45183f4c3b03e61c1c3dfa4713
layton <cheng_l@hotmail.com>
2026-04-02 23:53:39 -0400

    add mason-nvim-dap for nvim-dap automation

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,32 +1,49 @@
 return {
-	"rcarriga/nvim-dap-ui",
-	dependencies = {
-		"mfussenegger/nvim-dap",
-		"nvim-neotest/nvim-nio",
-		"leoluz/nvim-dap-go",
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"mfussenegger/nvim-dap",
+		},
+		config = function()
+			require("mason-nvim-dap").setup({
+				ensure_installed = {
+					"python",
+					"delve", -- for go
+				},
+			})
+		end,
 	},
-	config = function()
-		local dap = require("dap")
-		local dapui = require("dapui")
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = {
+			"mfussenegger/nvim-dap",
+			"nvim-neotest/nvim-nio",
+			"leoluz/nvim-dap-go",
+		},
+		config = function()
+			local dap = require("dap")
+			local dapui = require("dapui")
 
-		require("dapui").setup()
-		require("dap-go").setup()
+			require("dapui").setup()
+			require("dap-go").setup()
 
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
 
-		vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, {})
-		vim.keymap.set("n", "<Leader>dc", dap.continue, {})
-		vim.keymap.set("n", "<Leader>dt", dap.terminate, {})
-	end,
+			vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, {})
+			vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+			vim.keymap.set("n", "<Leader>dt", dap.terminate, {})
+		end,
+	},
 }
