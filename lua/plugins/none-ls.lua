e03e39ae297ab89abb038b3b71007d93ce835619
layton <cheng_l@hotmail.com>
2026-04-02 23:30:02 -0400

    add mason-null-ls for null-ls automation

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,25 +1,41 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"nvimtools/none-ls-extras.nvim",
+	{
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"nvimtools/none-ls.nvim",
+		},
+		config = function()
+			require("mason-null-ls").setup({
+				ensure_installed = { "stylua", "prettier", "black", "isort", "gofmt", "nginx_beautifier", "eslint_d", "beautysh" },
+			})
+		end,
 	},
-	config = function()
-		local null_ls = require("null-ls")
+	{
+		"nvimtools/none-ls.nvim",
+		dependencies = {
+			"nvimtools/none-ls-extras.nvim",
+		},
+		config = function()
+			local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua, -- for lua
-				null_ls.builtins.formatting.prettier.with({
-					extra_args = { "--trailing-comma", "none" },
-				}),               -- for vue, html, css ...
-				null_ls.builtins.formatting.black, -- for python
-				null_ls.builtins.formatting.isort, -- for python
-				null_ls.builtins.formatting.gofmt, -- for go
-				null_ls.builtins.formatting.nginx_beautifier, -- for nginx.conf
-				require("none-ls.diagnostics.eslint_d"), -- for typescript, javascript
-			},
-		})
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua, -- for lua
+					null_ls.builtins.formatting.prettier.with({
+						extra_args = { "--trailing-comma", "none" },
+					}), -- for vue, html, css ...
+					null_ls.builtins.formatting.black, -- for python
+					null_ls.builtins.formatting.isort, -- for python
+					null_ls.builtins.formatting.gofmt, -- for go
+					null_ls.builtins.formatting.nginx_beautifier, -- for nginx.conf
+					null_ls.builtins.formatting.beautysh,  -- for "bash", "csh", "ksh", "sh", "zsh"
+					require("none-ls.diagnostics.eslint_d"), -- for typescript, javascript
+				},
+			})
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		end,
+	},
 }
