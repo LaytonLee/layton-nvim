31bf1eeaa5bceeccef15dfae4a308a520aa7b814
layton <cheng_l@hotmail.com>
2026-04-08 11:23:25 -0400

    add custom markdown css style for markdown-preview.nvim

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,23 +1,13 @@
 return {
 	{
 		"iamcco/markdown-preview.nvim",
-		config = function()
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
 			vim.fn["mkdp#util#install"]()
-
-			local mkdp_preview_option = {
-				mkit = {},
-				katex = {},
-				uml = {},
-				maid = {},
-				disable_sync_scroll = 0,
-				sync_scroll_type = "middle",
-				hide_yaml_meta = 1,
-				sequence_diagrams = {},
-				flowchart_diagrams = {},
-				-- content_editable =
-				content_editable = 0,
-				toc = {},
-			}
+		end,
+		config = function()
+			vim.g.mkdp_markdown_css = vim.fn.stdpath("config") .. "/mkdp/markdown.css"
 		end,
 	},
 	{
